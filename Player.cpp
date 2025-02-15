91ff80e3a48f7a3379e2c868b38308781d794a7b
notkrithiramnath <krithiramnath@gmail.com>
2025-02-15 16:04:14 -0500

    Added stubs

---
+++ Player.cpp
@@ -0,0 +1,14 @@
+
+#include "Player.hpp"
+#include <string>
+#include <vector>
+#include <cassert>
+#include <iostream>
+#include <array>
+Player * Player_factory(const std::string &name, const std::string &strategy) {
+  assert(false);
+}
+
+std::ostream & operator<<(std::ostream &os, const Player &p) {
+  assert(false);
+}
